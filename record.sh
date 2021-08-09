56ff5462d39d8108a86a6f3f04088ff9eed71fbc
kuri.megane my_ubuntu <kuri.megane3060.4@gmail.com>
2021-08-09 17:20:45 +0900

    実行シェルを修正

--- record.sh
+++ record.sh
@@ -2,4 +2,4 @@
 
 THIS_FILE_PATH=$(cd $(dirname $0); pwd)
 cd "$THIS_FILE_PATH" || exit
-python3 "$THIS_FILE_PATH"/main.py >> "$THIS_FILE_PATH"/log/stdout.txt 2>&1
+/usr/bin/python3 "$THIS_FILE_PATH"/main.py
--- secrets.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# 環境変数を設定
-export LINE_NOTIFY_TOKEN=
