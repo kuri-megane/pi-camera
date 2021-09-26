77f41db52cdf96b441c450b027e132564f1a998e
kuri.megane my_ubuntu <kuri.megane3060.4@gmail.com>
2021-09-26 22:41:05 +0900

    シェルスクリプトにtry-catchを追加

--- record.sh
+++ record.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+set -eu
+
+function catch {
+  echo "シェル異常終了" >> "$THIS_FILE_PATH"/log/pi-camera.log
+}
+function finally {
+  echo "シェル正常終了" >> "$THIS_FILE_PATH"/log/pi-camera.log
+}
+trap catch ERR
+trap finally EXIT
 
 THIS_FILE_PATH=$(cd $(dirname $0); pwd)
 cd "$THIS_FILE_PATH" || exit
-timeout -sKILL 60 /usr/bin/python3 "$THIS_FILE_PATH"/main.py
+timeout -sKILL 60 /usr/bin/python3 "$THIS_FILE_PATH"/main.py >> "$THIS_FILE_PATH"/log/pi-camera.log 2>&1
